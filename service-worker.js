5caf9b48771c8ac314524eb0e914dff3b91390d5
Fenou-Joel <fenoujoelbodjrenou@gmail.com>
2025-08-07 17:31:25 +0100

    version web mobile

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'remise-cache-v1';
+const filesToCache = [
+  './',
+  './index1.html',
+  './index2.html',
+  './manifest.json',
+  './icons/icon.png',
+  './icons/icon-192.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
